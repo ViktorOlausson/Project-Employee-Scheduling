df984ce46da20feaf0f72e5c662340241796e2d1
tobiasburman <tobias.burman99@gmail.com>
2026-04-12 19:16:05 +0200

    feat: add navbar and routing

--- frontend/employee-scheduling/src/App.tsx
+++ frontend/employee-scheduling/src/App.tsx
@@ -1,24 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import EmployeeListPage from './pages/EmployeeListPage'
-import RegisterEmployeePage from './pages/RegisterEmployeePage'
-import JobSchedulePage from './pages/JobSchedulePage'
-import AvailabilityPage from './pages/AvailabilityPage'
-import MySchedulePage from './pages/MySchedulePage'
-import WorkSchedulePage from './pages/WorkSchedulePage'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import LoginPage from "./pages/LoginPage"
+import EmployeeListPage from "./pages/EmployeeListPage"
+import RegisterEmployeePage from "./pages/RegisterEmployeePage"
+import JobSchedulePage from "./pages/JobSchedulePage"
+import AvailabilityPage from "./pages/AvailabilityPage"
+import MySchedulePage from "./pages/MySchedulePage"
+import WorkSchedulePage from "./pages/WorkSchedulePage"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/employees" element={<EmployeeListPage />} />
-        <Route path="/employees/register" element={<RegisterEmployeePage />} />
-        <Route path="/schedule" element={<JobSchedulePage />} />
-        <Route path="/availability" element={<AvailabilityPage />} />
-        <Route path="/my-schedule" element={<MySchedulePage />} />
-        <Route path="/schedule/:employeeId" element={<WorkSchedulePage />} />
-
+        <Route path="/employees" element={<><Navbar /><EmployeeListPage /></>} />
+        <Route path="/employees/register" element={<><Navbar /><RegisterEmployeePage /></>} />
+        <Route path="/schedule" element={<><Navbar /><JobSchedulePage /></>} />
+        <Route path="/availability" element={<><Navbar /><AvailabilityPage /></>} />
+        <Route path="/my-schedule" element={<><Navbar /><MySchedulePage /></>} />
+        <Route path="/schedule/:employeeId" element={<><Navbar /><WorkSchedulePage /></>} />
       </Routes>
     </BrowserRouter>
   )
