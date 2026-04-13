3842f19f053b08f18312c0ccee06aa26058fb0f9
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-04-13 16:20:24 +0200

    logger.ts

--- backend/server.ts
+++ backend/server.ts
@@ -2,8 +2,8 @@ import express from "express";
 import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { loginSchema } from "./types/index.js";
-
 import "dotenv/config";
+import logger from "./logger.js";
 
 const app = express();
 app.use(express.json());
@@ -11,108 +11,120 @@ const PORT = 3000;
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
-	throw new Error("DATABASE_URL is not set");
+  logger.error("DATABASE_URL is not set");
+  throw new Error("DATABASE_URL is not set");
 }
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:3000`);
+  logger.info(`Server is running on http://localhost:3000`);
+  console.log(`Server is running on http://localhost:3000`);
 });
 
 app.get("/ping", (req, res) => {
-	res.json({ message: "pong" });
+  logger.info("ping/pong");
+  res.json({ message: "pong" });
 });
 
 app.get("/users/employees/all", async (req, res) => {
-	try {
-		const users = await prisma.user.findMany({
-			where: { role: "EMPLOYEE" },
-			select: {
-				id: true,
-				email: true,
-				firstName: true,
-				lastName: true,
-				Occupation: true,
-				role: true,
-			},
-		});
-		res.send(users);
-	} catch (err) {
-		if (err instanceof Error) {
-			res.status(500).json({
-				message: err.message,
-			});
-		}
-		res.status(500).send("Unknown error");
-	}
+  try {
+    const users = await prisma.user.findMany({
+      where: { role: "EMPLOYEE" },
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        Occupation: true,
+        role: true,
+      },
+    });
+    logger.info("fetch all users");
+    res.send(users);
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error(err);
+      res.status(500).json({
+        message: err.message,
+      });
+    }
+    res.status(500).send("Unknown error");
+  }
 });
 
 // -- Login with email and password and compare loginCode --
 app.post("/login", async (req, res) => {
-	try {
-		// Validate input
-		const result = loginSchema.safeParse(req.body);
-		if (!result.success) {
-			res.status(400).json({ error: result.error.message });
-			return;
-		}
-		const { email, password } = result.data;
+  try {
+    // Validate input
+    const result = loginSchema.safeParse(req.body);
+    if (!result.success) {
+      logger.error(result.error);
+      res.status(400).json({ error: result.error.message });
+      return;
+    }
+    const { email, password } = result.data;
 
-		// Find user by email
-		const user = await prisma.user.findUnique({
-			where: { email: email },
-		});
+    // Find user by email
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
 
-		// Check if user exists
-		if (!user) {
-			return res
-				.status(401)
-				.json({ error: "Invalid credentials, user do not exist" });
-		}
+    // Check if user exists
+    if (!user) {
+      logger.error("Invalid credentials, user do not exist");
+      return res
+        .status(401)
+        .json({ error: "Invalid credentials, user do not exist" });
+    }
 
-		// Compare loginCode
-		if (user.loginCode !== password) {
-			console.log(user.loginCode, password);
-			return res
-				.status(401)
-				.json({ error: "Invalid credentials, wrong loginCode" });
-		}
+    // Compare loginCode
+    if (user.loginCode !== password) {
+      console.log(user.loginCode, password);
+      logger.error("Invalid credentials, wrong loginCode");
+      return res
+        .status(401)
+        .json({ error: "Invalid credentials, wrong loginCode" });
+    }
 
-		// Success response
-		return res.status(200).json({
-			message: "Login successful",
-			userId: user.id,
-			name: user.firstName,
-		});
-	} catch (error) {
-		console.error("Login error:", error);
-		return res.status(500).json({ error: "Internal server error" });
-	}
+    // Success response
+    logger.info("Successful login");
+    return res.status(200).json({
+      message: "Login successful",
+      userId: user.id,
+      name: user.firstName,
+    });
+  } catch (error) {
+    logger.error(`Login error: ${error}`);
+    // console.error("Login error:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 //Get availablity and empolyeeID
 app.get("/availability/:employeeId", async (req, res) => {
   try {
-    const employeeId = parseInt(req.params.employeeId)
+    const employeeId = parseInt(req.params.employeeId);
     const availability = await prisma.availability.findMany({
-      where: {userId: employeeId},
-    })
+      where: { userId: employeeId },
+    });
 
-    res.status(200).json(availability)
+    res.status(200).json(availability);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" })
+    logger.error(error);
+    // console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
 app.put("/availability/:employeeId", async (req, res) => {
   try {
-    const employeeId = parseInt(req.params.employeeId)
-    const {date, shift} = req.body;
+    const employeeId = parseInt(req.params.employeeId);
+    const { date, shift } = req.body;
 
-    const availability = await prisma.availability.upsert({  //upsert är som if else, if (om where finns, updatera, annars skapa en)
+    const availability = await prisma.availability.upsert({
+      //upsert är som if else, if (om where finns, updatera, annars skapa en)
       where: {
         userId_date_shift: {
           userId: employeeId,
@@ -126,10 +138,11 @@ app.put("/availability/:employeeId", async (req, res) => {
         date: new Date(date),
         shift: shift,
       },
-    })
-    res.status(200).json(availability)
+    });
+    res.status(200).json(availability);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" })
+    logger.error(error);
+    // console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
-})
+});
