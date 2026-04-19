1d48ccf613e965c0dfc5592d5908d93f84481b65
maria-techhub <mariakiran.tech@gmail.com>
2026-04-19 22:39:07 +0200

    Delete Employee

--- frontend/employee-scheduling/src/components/EmployeeCard.tsx
+++ frontend/employee-scheduling/src/components/EmployeeCard.tsx
@@ -1,10 +1,18 @@
 import type { Employee } from "../types/scheduleTypes"
+import { useDeleteEmployee } from "../hooks/useDeleteEmployee";
 
 interface Props {
     employee: Employee
 }
 
 const EmployeeCard = ({ employee }: Props) => {
+    const { mutate: deleteEmp } = useDeleteEmployee();
+
+	const handleDelete = () => {
+		if (confirm("Are you sure you want to delete this employee?")) {
+			deleteEmp(employee.id);
+		}
+	};
     return (
         <div className="bg-white rounded shadow p-4 flex items-center gap-4">
             <div className="bg-gray-200 rounded-full w-12 h-12 flex items-center justify-center text-lg font-bold">
@@ -15,6 +23,12 @@ const EmployeeCard = ({ employee }: Props) => {
                 <p className="text-sm text-gray-500">{employee.email}</p>
                 <p className="text-sm text-gray-400">{employee.Occupation}</p>
             </div>
+            <button
+				onClick={handleDelete}
+				className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+			>
+				Delete
+			</button>
         </div>
     )
 }
